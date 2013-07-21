823df11b78f21921f16428f74a0632f51914a127
wyf1013 <wyf1013@gmail.com>
2013-07-21 01:45:30 +0000

    web.js now read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Again... This time updated from cmd line...');
+  //response.send('Hello World! Again... This time updated from cmd line...');
+    response.send(fs.readFileSync(index.html,encoding));
 });
 
 var port = process.env.PORT || 5000;
